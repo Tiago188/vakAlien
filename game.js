591254f5644f4fc56528144de2ef6a49505f5f6c
Tiago S. Oliveira <TiagoProff@gmail.com>
2020-02-18 20:47:10 -0300

    Update game

--- game.js
+++ game.js
@@ -1,3 +1,21 @@
-import KMG from './km/kmg';
+import KMG from './km/kmg.js';
+import Game from './km/Game.js';
+
+let resources = {
+    personSmile: './assets/image/smiley.gif',
+    personAngry: './assets/image/angry.gif'
+};
 
 let game = new KMG.Game();
+
+const person = new KMG.Sprite(resources.personSmile);
+
+game.stage.addChild(person);
+
+/*
+// Listen for frame updates
+app.ticker.add(() => {
+    // each frame we spin the bunny around a bit
+    person.rotation += 0.01;
+});
+*/
