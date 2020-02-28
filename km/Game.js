b9dcb1cc22dcc7c325f69aee295d2fe8044dfcb7
Tiago S. Oliveira <TiagoProff@gmail.com>
2020-02-28 14:21:09 -0300

    Add tick

--- km/Game.js
+++ km/Game.js
@@ -1,3 +1,4 @@
+import KMG from "./kmg.js";
 import Container from "./Container.js";
 
 export default class Game 
@@ -5,6 +6,8 @@ export default class Game
     constructor () // singleton
     {
         this.stage = new Container();
+        this.tickState = 'stop';
+        this.tickers = [];
     }
 
     addScene () 
@@ -17,10 +20,27 @@ export default class Game
         //
     }
 
+    addTick ( tick ) 
+    {
+        this.tickers.push(tick);
+
+        if ( this.tickState === 'stop' ) 
+            this.tick();
+    }
+
+    tick () 
+    {
+        for ( const i in this.tickers ) 
+            this.tickers[i]();
+
+        this.render();
+        requestAnimationFrame(()=>this.tick());
+    }
+
     render () 
     {
         const ObjectsRenders = this.stage.children;
-        this.screenClear();
+        KMG.screenClear();
     
         for ( const i in ObjectsRenders ) 
             ObjectsRenders[i].render();
